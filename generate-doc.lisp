98bd3241f79a4b0893fa2375929f98b78c376ce6
Ralph Schleicher <rs@ralph-schleicher.de>
2021-10-10 17:57:26 +0000

    Omit generic functions.

--- generate-doc.lisp
+++ generate-doc.lisp
@@ -38,40 +38,39 @@
 (ql:quickload :open-location-code)
 (ql:quickload :rs-doc) ;private
 
-(let ((symbols '(olc:validp
-		 olc:fullp
-		 olc:shortp
-		 olc:encode
-		 olc:decode
-		 olc:shorten
-		 olc:recover
-		 olc:code-error
-		 olc:code-length-error
-		 olc:invalid-code-error
-		 olc:full-code-error
-		 olc:short-code-error
-		 olc:code-area
-		 olc:south-west-corner
-		 olc:north-east-corner
-		 olc:center
-		 olc:precision
-		 olc:code-length
-		 olc:separator-position
-		 olc:pad-characters)))
-  (rs-doc:generate-doc
-   :package :open-location-code
-   :symbols symbols
-   :output-format :html
-   :output (make-pathname :directory '(:relative "doc")
-			  :name "open-location-code"
-			  :type "html"))
-  (rs-doc:generate-doc
-   :package :open-location-code
-   :symbols symbols
-   :output-format :text
-   :output (make-pathname :directory '(:relative "doc")
-			  :name "open-location-code"
-			  :type "txt"))
+(let ((doc (rs-doc:gather-doc
+	    :package :open-location-code
+	    :symbols '(olc:validp
+		       olc:fullp
+		       olc:shortp
+		       olc:encode
+		       olc:decode
+		       olc:shorten
+		       olc:recover
+		       olc:code-error
+		       olc:code-length-error
+		       olc:invalid-code-error
+		       olc:full-code-error
+		       olc:short-code-error
+		       olc:code-area
+		       olc:south-west-corner
+		       olc:north-east-corner
+		       olc:center
+		       olc:precision
+		       olc:code-length
+		       olc:separator-position
+		       olc:pad-characters)
+	    :generic-functions nil)))
+  (rs-doc:generate-doc :data doc
+		       :output-format :html
+		       :output (make-pathname :directory '(:relative "doc")
+					      :name "open-location-code"
+					      :type "html"))
+  (rs-doc:generate-doc :data doc
+		       :output-format :text
+		       :output (make-pathname :directory '(:relative "doc")
+					      :name "open-location-code"
+					      :type "txt"))
   ())
 
 ;;; generate-doc.lisp ends here
